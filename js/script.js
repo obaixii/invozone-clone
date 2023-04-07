00d3e172da9521436be498ebf0be5c45da8f52da
Obaid Awan <80578527+obaixii@users.noreply.github.com>
2023-04-07 23:42:33 +0500

    Invozone Clone

---
+++ js/script.js
@@ -0,0 +1,127 @@
+
+
+ ///// PORT CAROUSEL /////
+$('.slider').slick({
+    margin: 20,
+    dots: true,
+    infinite: true,
+    speed: 700,
+    autoplay: true,
+    arrows:true,
+    // fade: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+ ///// CONTACT slider1 /////
+ $('.slider1').slick({
+    margin: 20,
+    dots: false,
+    infinite: true,
+    speed: 700,
+    autoplay: true,
+    arrows:true,
+    // fade: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+ ///// CONTACT slider2 /////
+ $('.slider2').slick({
+    margin: 20,
+    dots: false,
+    infinite: true,
+    speed: 700,
+    autoplay: true,
+    arrows:true,
+    // fade: true,
+    // autoplaySpeed: 4000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
